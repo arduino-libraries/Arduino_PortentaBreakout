8a49d2ecf77555709ff945581680c98c2f614c8a
Riccardo Rizzo <r.rizzo@arduino.cc>
2021-03-25 15:30:14 +0100

    modified APIs names and minor fix

--- PortentaBreakoutCarrier.h
+++ PortentaBreakoutCarrier.h
@@ -25,24 +25,27 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
 #ifndef _BREAKOUT_CARRIER_H
 #define _BREAKOUT_CARRIER_H
+
 #include "Arduino.h"
+#include "pins_arduino.h"
 #include "mbed.h"
-#define PK_7_pox LEDB
+
+#define LAST_ARDUINO_PIN_NUMBER LEDB + 1
 typedef enum {
     SPI0_CS = -1,
     SPI0_CK = -1,
     SPI0_MISO = -1,
     SPI0_MOSI = -1,
-    SPI1_CS = PK_7_pox + 129,
-    SPI1_CK = PK_7_pox + 130,
-    SPI1_MISO = PK_7_pox + 35,
-    SPI1_MOSI = PK_7_pox + 36,
-    UART2_TX = PK_7_pox + 111,
-    UART2_RX = PK_7_pox+ 106,
+    SPI1_CS = LAST_ARDUINO_PIN_NUMBER + PI_0,
+    SPI1_CK = LAST_ARDUINO_PIN_NUMBER + PI_1,
+    SPI1_MISO = LAST_ARDUINO_PIN_NUMBER + PC_2,
+    SPI1_MOSI = LAST_ARDUINO_PIN_NUMBER + PC_3,
+    UART2_TX = LAST_ARDUINO_PIN_NUMBER + PG_14,
+    UART2_RX = LAST_ARDUINO_PIN_NUMBER + PG_9,
     UART2_RTS = -1,
     UART2_CTS = -1,
-    UART3_TX = PK_7_pox + 153,
-    UART3_RX = PK_7_pox + 154,
+    UART3_TX = LAST_ARDUINO_PIN_NUMBER + PJ_8,
+    UART3_RX = LAST_ARDUINO_PIN_NUMBER + PJ_9,
     UART3_RTS = -1,
     UART3_CTS = -1,
     PCIE_TXN = -1,
@@ -51,44 +54,44 @@ typedef enum {
     PCIE_RXP = -1,
     PCIE_CKN = -1,
     PCIE_CKP = -1,
-    I2S_SDO = PK_7_pox + 132,
-    I2S_SDI = PK_7_pox + 131,
-    I2S_WS = PK_7_pox + 26,
-    I2S_CK = PK_7_pox + 52,
+    I2S_SDO = LAST_ARDUINO_PIN_NUMBER + PI_3,
+    I2S_SDI = LAST_ARDUINO_PIN_NUMBER + PI_2,
+    I2S_WS = LAST_ARDUINO_PIN_NUMBER + PB_9,
+    I2S_CK = LAST_ARDUINO_PIN_NUMBER + PD_3,
     SAI_D1 = -1,
-    SAI_D0 = PK_7_pox + 135,
-    SAI_FS = PK_7_pox + 136,
-    SAI_SCK = PK_7_pox + 134,
-    CAMERA_D0P = PK_7_pox + 123,
-    CAMERA_D0N = PK_7_pox + 122,
-    CAMERA_D1P = PK_7_pox + 125,
-    CAMERA_D1N = PK_7_pox + 124,
-    CAMERA_D2P = PK_7_pox + 133,
-    CAMERA_D2N = PK_7_pox + 127,
-    CAMERA_D3P = PK_7_pox + 136,
-    CAMERA_D3N = PK_7_pox + 135,
-    CAMERA_CKP = PK_7_pox + 134,
-    CAMERA_CKN = PK_7_pox + 7,
-    CAMERA_HS = PK_7_pox + 5,
+    SAI_D0 = LAST_ARDUINO_PIN_NUMBER + PI_6,
+    SAI_FS = LAST_ARDUINO_PIN_NUMBER + PI_7,
+    SAI_SCK = LAST_ARDUINO_PIN_NUMBER + PI_5,
+    CAMERA_D0P = LAST_ARDUINO_PIN_NUMBER + PH_10,
+    CAMERA_D0N = LAST_ARDUINO_PIN_NUMBER + PH_9,
+    CAMERA_D1P = LAST_ARDUINO_PIN_NUMBER + PH_12,
+    CAMERA_D1N = LAST_ARDUINO_PIN_NUMBER + PH_11,
+    CAMERA_D2P = LAST_ARDUINO_PIN_NUMBER + PI_4,
+    CAMERA_D2N = LAST_ARDUINO_PIN_NUMBER + PH_14,
+    CAMERA_D3P = LAST_ARDUINO_PIN_NUMBER + PI_7,
+    CAMERA_D3N = LAST_ARDUINO_PIN_NUMBER + PI_6,
+    CAMERA_CKP = LAST_ARDUINO_PIN_NUMBER + PI_5,
+    CAMERA_CKN = LAST_ARDUINO_PIN_NUMBER + PA_6,
+    CAMERA_HS = LAST_ARDUINO_PIN_NUMBER + PA_4,
     PDM_D1 = -1,
-    PDM_D0 = PK_7_pox + 19,
-    PDM_CK = PK_7_pox + 67,
+    PDM_D0 = LAST_ARDUINO_PIN_NUMBER + PB_2,
+    PDM_CK = LAST_ARDUINO_PIN_NUMBER + PE_2,
     SPDIF_RX = -1,
     SPDIF_TX= -1,
-    USBHS_ID = PK_7_pox + 151,
+    USBHS_ID = LAST_ARDUINO_PIN_NUMBER + PJ_6,
     USBHS_DN = -1,
     USBHS_DP = -1,
     USBFS_ID = -1,
-    USBFS_DN = PK_7_pox + 13,
-    USBFS_DP = PK_7_pox + 12,
+    USBFS_DN = LAST_ARDUINO_PIN_NUMBER + PA_12,
+    USBFS_DP = LAST_ARDUINO_PIN_NUMBER + PA_11,
     SD_WP = -1,
     SD_CD = -1,
-    SD_D0 = PK_7_pox + 31,
-    SD_D3  = PK_7_pox + 21,
-    SD_CMD = PK_7_pox + 56,
-    SD_D2 = PK_7_pox + 20,
-    SD_CLK = PK_7_pox + 55,
-    SD_D1 = PK_7_pox + 32,
+    SD_D0 = LAST_ARDUINO_PIN_NUMBER + PB_14,
+    SD_D3  = LAST_ARDUINO_PIN_NUMBER + PB_4,
+    SD_CMD = LAST_ARDUINO_PIN_NUMBER + PD_7,
+    SD_D2 = LAST_ARDUINO_PIN_NUMBER + PB_3,
+    SD_CLK = LAST_ARDUINO_PIN_NUMBER + PD_6,
+    SD_D1 = LAST_ARDUINO_PIN_NUMBER + PB_15,
     SD_VSD = -1,
     ETHERNET_DP = -1,
     ETHERNET_DN = -1,
@@ -100,14 +103,14 @@ typedef enum {
     ETHERNET_AP = -1,
     ETHERNET_L2 = -1,
     ETHERNET_L1 = -1,
-    UART0_TX = PK_7_pox + 1,
-    UART0_RX = PK_7_pox + 138,
-    UART0_RTS = PK_7_pox + 139,
-    UART0_CTS = PK_7_pox + 142,
-    UART1_TX = PK_7_pox + 10,
-    UART1_RX = PK_7_pox + 11,
-    UART1_RTS = PK_7_pox + 143,
-    UART1_CTS = PK_7_pox + 144,
+    UART0_TX = LAST_ARDUINO_PIN_NUMBER + PA_0,
+    UART0_RX = LAST_ARDUINO_PIN_NUMBER + PI_9,
+    UART0_RTS = LAST_ARDUINO_PIN_NUMBER + PI_10,
+    UART0_CTS = LAST_ARDUINO_PIN_NUMBER + PI_13,
+    UART1_TX = LAST_ARDUINO_PIN_NUMBER + PA_9,
+    UART1_RX = LAST_ARDUINO_PIN_NUMBER + PA_10,
+    UART1_RTS = LAST_ARDUINO_PIN_NUMBER + PI_14,
+    UART1_CTS = LAST_ARDUINO_PIN_NUMBER + PI_15,
     DISPLAY_D3P = -1,
     DISPLAY_D3N = -1,
     DISPLAY_D2P = -1,
@@ -120,75 +123,68 @@ typedef enum {
     DISPLAY_CLK_N = -1,
     CAN0_TX = -1,
     CAN0_RX = -1,
-    CAN1_TX = PK_7_pox + 126,
-    CAN1_RX = PK_7_pox + 25,
-    I2C_SDA_1 = PK_7_pox + 24,
-    I2C_SCL_1 = PK_7_pox + 23,
-    I2C_SDA_0 = PK_7_pox + 121,
-    I2C_SCL_0 = PK_7_pox + 120,
-    I2C_SDA_2 = PK_7_pox + 125,
-    I2C_SCL_2 = PK_7_pox + 124,
-    GPIO_0 = PK_7_pox + 46,
-    GPIO_1 = PK_7_pox + 48,
-    GPIO_2 = PK_7_pox + 53,
-    GPIO_3 = PK_7_pox + 54,
-    GPIO_4 = PK_7_pox + 68,
-    GPIO_5 = PK_7_pox + 100,
-    GPIO_6 = PK_7_pox + 107,
+    CAN1_TX = LAST_ARDUINO_PIN_NUMBER + PH_13,
+    CAN1_RX = LAST_ARDUINO_PIN_NUMBER + PB_8,
+    I2C_SDA_1 = LAST_ARDUINO_PIN_NUMBER + PB_7,
+    I2C_SCL_1 = LAST_ARDUINO_PIN_NUMBER + PB_6,
+    I2C_SDA_0 = LAST_ARDUINO_PIN_NUMBER + PH_8,
+    I2C_SCL_0 = LAST_ARDUINO_PIN_NUMBER + PH_7,
+    I2C_SDA_2 = LAST_ARDUINO_PIN_NUMBER + PH_12,
+    I2C_SCL_2 = LAST_ARDUINO_PIN_NUMBER + PH_11,
+    GPIO_0 = LAST_ARDUINO_PIN_NUMBER + PC_13,
+    GPIO_1 = LAST_ARDUINO_PIN_NUMBER + PC_15,
+    GPIO_2 = LAST_ARDUINO_PIN_NUMBER + PD_4,
+    GPIO_3 = LAST_ARDUINO_PIN_NUMBER + PD_5,
+    GPIO_4 = LAST_ARDUINO_PIN_NUMBER + PE_3,
+    GPIO_5 = LAST_ARDUINO_PIN_NUMBER + PG_3,
+    GPIO_6 = LAST_ARDUINO_PIN_NUMBER + PG_10,
     ANALOG_REFN = -1,
     ANALOG_REFP = -1,
-    ANALOG_A7 = PK_7_pox + 7,
-    ANALOG_A6 = PK_7_pox + 5,
-    ANALOG_A5 = PK_7_pox + 36,
-    ANALOG_A4 = PK_7_pox + 35,
-    ANALOG_A3 = PK_7_pox + 36,
-    ANALOG_A2 = PK_7_pox + 35,
-    ANALOG_A1 = PK_7_pox + 2,
-    ANALOG_A0 = PK_7_pox + 1,
-    PWM9 = PK_7_pox + 119,
-    PWM8 = PK_7_pox + 155,
-    PWM7 = PK_7_pox + 152,
-    PWM6 = PK_7_pox + 128,
-    PWM5 = PK_7_pox + 162,
-    PWM4 = PK_7_pox + 156,
-    PWM3 = PK_7_pox + 104,
-    PWM2 = PK_7_pox + 39,
-    PWM1 = PK_7_pox + 38,
-    PWM0 = PK_7_pox + 9,
+    ANALOG_A7 = LAST_ARDUINO_PIN_NUMBER + PA_6,
+    ANALOG_A6 = LAST_ARDUINO_PIN_NUMBER + PA_4,
+    ANALOG_A5 = LAST_ARDUINO_PIN_NUMBER + PC_3,
+    ANALOG_A4 = LAST_ARDUINO_PIN_NUMBER + PC_2,
+    ANALOG_A3 = LAST_ARDUINO_PIN_NUMBER + PC_3,
+    ANALOG_A2 = LAST_ARDUINO_PIN_NUMBER + PC_2,
+    ANALOG_A1 = LAST_ARDUINO_PIN_NUMBER + PA_1,
+    ANALOG_A0 = LAST_ARDUINO_PIN_NUMBER + PA_0,
+    PWM9 = LAST_ARDUINO_PIN_NUMBER + PH_6,
+    PWM8 = LAST_ARDUINO_PIN_NUMBER + PJ_10,
+    PWM7 = LAST_ARDUINO_PIN_NUMBER + PJ_7,
+    PWM6 = LAST_ARDUINO_PIN_NUMBER + PH_15,
+    PWM5 = LAST_ARDUINO_PIN_NUMBER + PK_1,
+    PWM4 = LAST_ARDUINO_PIN_NUMBER + PJ_11,
+    PWM3 = LAST_ARDUINO_PIN_NUMBER + PG_7,
+    PWM2 = LAST_ARDUINO_PIN_NUMBER + PC_7,
+    PWM1 = LAST_ARDUINO_PIN_NUMBER + PC_6,
+    PWM0 = LAST_ARDUINO_PIN_NUMBER + PA_8,
     TRST = -1,
-    TRACEDATA_0 = PK_7_pox + 68,
-    TRACEDATA_1 = PK_7_pox + 111,
-    TRACEDATA_CLK = PK_7_pox + 67,
+    TRACEDATA_0 = LAST_ARDUINO_PIN_NUMBER + PE_3,
+    TRACEDATA_1 = LAST_ARDUINO_PIN_NUMBER + PG_14,
+    TRACEDATA_CLK = LAST_ARDUINO_PIN_NUMBER + PE_2,
     USB_FLAG = -1,
     USB_EN = -1
-};
+} breakoutPin;
 
 class BreakoutCarrierClass {
 public:
-    pin_size_t getPin(int pin) {
+    int pinMode(breakoutPin pin, PinMode mode) {
         if (pin > -1) {
-            return digitalPinToPinName(pin);
-        }
-        return 0;
-    }
-
-    int setPinMode(int pin, PinMode mode) {
-        if (pin > -1) {
-            pinMode(digitalPinToPinName(pin), mode);
+            ::pinMode((int)pin, mode);
             return 1;
         }
         return -1;
     }
-    int GPIOWrite(int pin, PinStatus status) {
+    int digitalWrite(breakoutPin pin, PinStatus status) {
           if (pin > -1) {
-            digitalWrite(digitalPinToPinName(pin), status);
+            ::digitalWrite((int)pin, status);
             return 1;
         }
         return -1;
     }
-    bool GPIORead(int pin){
-           if (pin > -1) {
-            return digitalRead(digitalPinToPinName(pin));
+    bool digitalRead(breakoutPin pin){
+        if (pin > -1) {
+            return ::digitalRead((int)pin);
         }
         return -1;
     }
--- examples/GpioManagement/GpioManagement.ino
+++ examples/GpioManagement/GpioManagement.ino
@@ -13,33 +13,35 @@
 */
 #include "PortentaBreakoutCarrier.h"
 
-// the macros to access each gpio are made using the peripheral silk name 
-// and the pin Name(e.g. the GPIO 0 pin can be accessed using 
+// The macros to access each gpio are made using the peripheral silk name
+// and the pin Name (e.g. the GPIO 0 pin can be accessed using
 // GPIO_0), all the pin respect this naming except the followings:
 // I2C: I2C_SDA_1, I2C_SCL_1, I2C_SDA_0, I2C_SCL_0, I2C_SDA_2, I2C_SCL_2
 // PWM: PWM9, PWM8, PWM7, PWM6, PWM5, PWM4, PWM3, PWM2, PWM1, PWM0
-// not all the PINS are writatble, for pins availablity refers to:
+// not all the PINS are writatble and not all PINS are connected to the header,
+// for pins availablity refers to:
 // https://content.arduino.cc/assets/Pinout_PortentaBreakout_latest.pdf
 
-int pin = SD_CMD;
+breakoutPin pin = ANALOG_A7;
+
 void setup() {
   Serial.begin(9600);
   while (!Serial);
-
-  int res = Breakout.setPinMode(pin, OUTPUT);
-  if (res == -1){
-    Serila.println("Error: the pin is not connected to the Breakout pins");
+  int res = Breakout.pinMode(pin, OUTPUT);
+  if (res == -1) {
+    Serial.println("Error: pin not connected");
+    while(1);
   }
-  Breakout.GPIOWrite(pin, LOW);
+  Breakout.digitalWrite(pin, LOW);
 }
 
 void loop() {
-  Breakout.GPIOWrite(pin, HIGH);
-  Serial.println("Read: ");
-  Serial.println(Breakout.GPIORead(pin));
+  Breakout.digitalWrite(pin, HIGH);
+  Serial.print("Read: ");
+  Serial.println(Breakout.digitalRead(pin));
   delay(1000);
-  Breakout.GPIOWrite(pin, LOW);
-   Serial.println("Read: ");
-  Serial.println(Breakout.GPIORead(pin));
+  Breakout.digitalWrite(pin, LOW);
+  Serial.print("Read: ");
+  Serial.println(Breakout.digitalRead(pin));
   delay(1000);
 }
