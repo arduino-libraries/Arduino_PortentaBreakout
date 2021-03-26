b2cb316bd9a94b6c473483f22a54e01474e546f9
per1234 <accounts@perglass.com>
2021-03-26 00:22:37 -0700

    Correct typos in example sketch comments

--- examples/GpioManagement/GpioManagement.ino
+++ examples/GpioManagement/GpioManagement.ino
@@ -1,25 +1,25 @@
 /*
-  Breakout Carrier - Gpio Management
+  Breakout Carrier - GPIO Management
 
   The sketch shows how to use the Breakout Carrier
-  and how to turns a PIN on for one second, then off for one
+  and how to turn a PIN on for one second, then off for one
   second, repeatedly.
 
   The circuit:
    - Portenta H7
-   - BreakOut Carrier
+   - Portenta Breakout Carrier
 
   This example code is in the public domain.
 */
 #include "PortentaBreakoutCarrier.h"
 
-// The macros to access each gpio are made using the peripheral silk name
-// and the pin Name (e.g. the GPIO 0 pin can be accessed using
-// GPIO_0), all the pin respect this naming except the followings:
+// The macros to access each GPIO are made using the peripheral silk name
+// and the pin name (e.g. the GPIO 0 pin can be accessed using
+// GPIO_0), all the pins respect this naming except the following:
 // I2C: I2C_SDA_1, I2C_SCL_1, I2C_SDA_0, I2C_SCL_0, I2C_SDA_2, I2C_SCL_2
 // PWM: PWM9, PWM8, PWM7, PWM6, PWM5, PWM4, PWM3, PWM2, PWM1, PWM0
-// not all the PINS are writatble and not all PINS are connected to the header,
-// for pins availablity refers to:
+// Not all the PINS are writable and not all PINS are connected to the header.
+// For pins availability refer to:
 // https://content.arduino.cc/assets/Pinout_PortentaBreakout_latest.pdf
 
 breakoutPin pin = ANALOG_A7;
